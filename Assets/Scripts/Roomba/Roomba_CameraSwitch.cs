74e2dd421ccd7fbd85480b7bbf7a908037bf11c5
Mani <itsamanihd@gmail.com>
2025-12-15 16:03:14 +0000

    Updated Camera Switch
    Instructions commented

--- Assets/Scripts/Roomba/Roomba_CameraSwitch.cs
+++ Assets/Scripts/Roomba/Roomba_CameraSwitch.cs
@@ -5,6 +5,8 @@ public class Roomba_CameraSwitch : MonoBehaviour
 {
     // === ROOMBA_CAMERASWITCH SCRIPT === \\
     // This script switches the active camera when the player enters or exits the area. \\
+    // Place this script in the trigger area where you want the camera switch to occur. \\
+    // Use Ctrl + Shift + F to focus on the selected camera in the scene view. \\
     // __________________________________________\\
 
 
